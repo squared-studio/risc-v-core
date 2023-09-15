93b0adbd07225fd2e1dd49b92f1395944ecd661c
Foez Ahmed <foez.official@gmail.com>
2023-09-15 20:51:06 +0600

    upto 0x37 done modified:   tb_func_decode.sv

--- tb_func_decode.sv
+++ tb_func_decode.sv
@@ -194,17 +194,6 @@ module tb_func_decode;
   } decoded_inst_t;
 
   //  OPCODE  func rs1 rs2 rs3 rd imm
-  //  7'h03     .   .   .   .   .  .
-  //  7'h07     .   .   .   .   .  .
-  //  7'h0F     .   .   .   .   .  .
-  //  7'h13     .   .   .   .   .  .
-  //  7'h17     x   x   x   x   x  x
-  //  7'h1B     .   .   .   .   .  .
-  //  7'h23     .   .   .   .   .  .
-  //  7'h27     .   .   .   .   .  .
-  //  7'h2F     .   .   .   .   .  .
-  //  7'h33     .   .   .   .   .  .
-  //  7'h37     x   x   x   x   x  x
   //  7'h3B     .   .   .   .   .  .
   //  7'h43     .   .   .   .   .  .
   //  7'h47     .   .   .   .   .  .
@@ -221,42 +210,186 @@ module tb_func_decode;
     decode = '0;
     case (instr[6:0])
       7'h03: begin
+        case (instr[14:12])
+          3'b000:  decode.func = LB;
+          3'b001:  decode.func = LH;
+          3'b010:  decode.func = LW;
+          3'b011:  decode.func = LD;
+          3'b100:  decode.func = LBU;
+          3'b101:  decode.func = LHU;
+          3'b110:  decode.func = LWU;
+          default: return '0;
+        endcase
+        decode.rs1       = instr[19:15];
+        decode.rd        = instr[11:7];
+        decode.imm[11:0] = instr[31:20];
       end
 
       7'h07: begin
+        case (instr[14:12])
+          3'b010:  decode.func = FLW;
+          3'b011:  decode.func = FLD;
+          3'b100:  decode.func = FLQ;
+          default: return '0;
+        endcase
+        decode.rs1       = instr[19:15];
+        decode.rd        = instr[11:7];
+        decode.imm[11:0] = instr[31:20];
       end
 
       7'h0F: begin
+        case (instr[14:12])
+          3'b001:  decode.func = FENCE;
+          default: return '0;
+        endcase
+        decode.rs1       = instr[19:15];
+        decode.rd        = instr[11:7];
+        decode.imm[11:0] = instr[31:20];
       end
 
       7'h13: begin
+        case (instr[14:12])
+          3'b000: decode.func = ADDI;
+          3'b010: decode.func = SLTI;
+          3'b011: decode.func = SLTIU;
+          3'b100: decode.func = XORI;
+          3'b110: decode.func = ORI;
+          3'b111: decode.func = ANDI;
+          default: begin
+            case ({
+              instr[31:26], instr[14:12]
+            })
+              9'b000000_001: decode.func = SLLI;
+              9'b000000_101: decode.func = SRLI;
+              9'b010000_101: decode.func = SRAI;
+              default: return '0;
+            endcase
+          end
+        endcase
+        decode.rs1       = instr[19:15];
+        decode.rd        = instr[11:7];
+        decode.imm[11:0] = instr[31:20];
       end
 
       7'h17: begin
+        decode.func       = AUIPC;
         decode.rd         = instr[11:7];
         decode.imm[31:12] = instr[31:12];
-        decode.func       = AUIPC;
       end
 
       7'h1B: begin
+        case (instr[14:12])
+          3'b000: decode.func = ADDIW;
+          default: begin
+            case ({
+              instr[31:26], instr[14:12]
+            })
+              9'b000000_001: decode.func = SLLIW;
+              9'b000000_101: decode.func = SRLIW;
+              9'b010000_101: decode.func = SRAIW;
+              default: return '0;
+            endcase
+          end
+        endcase
+        decode.rs1       = instr[19:15];
+        decode.rd        = instr[11:7];
+        decode.imm[11:0] = instr[31:20];
       end
 
       7'h23: begin
+        case (instr[14:12])
+          3'b000:  decode.func = SB;
+          3'b001:  decode.func = SH;
+          3'b010:  decode.func = SW;
+          3'b011:  decode.func = SD;
+          default: return '0;
+        endcase
+        decode.rs1       = instr[19:15];
+        decode.rs2       = instr[24:20];
+        decode.imm[11:5] = instr[31:25];
+        decode.imm[4:0]  = instr[11:7];
       end
 
       7'h27: begin
+        case (instr[14:12])
+          3'b010:  decode.func = FSW;
+          3'b011:  decode.func = FSD;
+          3'b100:  decode.func = FSQ;
+          default: return '0;
+        endcase
+        decode.rs1       = instr[19:15];
+        decode.rs2       = instr[24:20];
+        decode.imm[11:5] = instr[31:25];
+        decode.imm[4:0]  = instr[11:7];
       end
 
       7'h2F: begin
+        case ({
+          instr[31:27], instr[14:12]
+        })
+          8'b00010_010: decode.func = LR_W;
+          8'b00011_010: decode.func = SC_W;
+          8'b00001_010: decode.func = AMOSWAP_W;
+          8'b00000_010: decode.func = AMOADD_W;
+          8'b00100_010: decode.func = AMOXOR_W;
+          8'b01100_010: decode.func = AMOAND_W;
+          8'b01000_010: decode.func = AMOOR_W;
+          8'b10000_010: decode.func = AMOMIN_W;
+          8'b10100_010: decode.func = AMOMAX_W;
+          8'b11000_010: decode.func = AMOMINU_W;
+          8'b11100_010: decode.func = AMOMAXU_W;
+          8'b00010_011: decode.func = LR_D;
+          8'b00011_011: decode.func = SC_D;
+          8'b00001_011: decode.func = AMOSWAP_D;
+          8'b00000_011: decode.func = AMOADD_D;
+          8'b00100_011: decode.func = AMOXOR_D;
+          8'b01100_011: decode.func = AMOAND_D;
+          8'b01000_011: decode.func = AMOOR_D;
+          8'b10000_011: decode.func = AMOMIN_D;
+          8'b10100_011: decode.func = AMOMAX_D;
+          8'b11000_011: decode.func = AMOMINU_D;
+          8'b11100_011: decode.func = AMOMAXU_D;
+          default: return '0;
+        endcase
+        decode.rs1      = instr[19:15];
+        decode.rs2      = instr[24:20];
+        decode.rd       = instr[11:7];
+        decode.imm[1:0] = instr[26:25];
       end
 
       7'h33: begin
+        case ({
+          instr[31:25], instr[14:12]
+        })
+          10'b0000000_000: decode.func = ADD;
+          10'b0100000_000: decode.func = SUB;
+          10'b0000000_001: decode.func = SLL;
+          10'b0000000_010: decode.func = SLT;
+          10'b0000000_011: decode.func = SLTU;
+          10'b0000000_100: decode.func = XOR;
+          10'b0000000_101: decode.func = SRL;
+          10'b0100000_101: decode.func = SRA;
+          10'b0000000_110: decode.func = OR;
+          10'b0000000_111: decode.func = AND;
+          10'b0000001_000: decode.func = MUL;
+          10'b0000001_001: decode.func = MULH;
+          10'b0000001_010: decode.func = MULHSU;
+          10'b0000001_011: decode.func = MULHU;
+          10'b0000001_100: decode.func = DIV;
+          10'b0000001_101: decode.func = DIVU;
+          10'b0000001_110: decode.func = REM;
+          10'b0000001_111: decode.func = REMU;
+          default: return '0;
+        endcase
+        decode.rs1 = instr[19:15];
+        decode.rs2 = instr[24:20];
+        decode.rd  = instr[11:7];
       end
 
       7'h37: begin
+        decode.func       = LUI;
         decode.rd         = instr[11:7];
         decode.imm[31:12] = instr[31:12];
-        decode.func       = LUI;
       end
 
       7'h3B: begin
