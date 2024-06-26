5243c903ad18785f83d6df26a7827b24a46c9f8e
Foez Ahmed <foez.official@gmail.com>
2024-06-26 13:11:01 +0600

    modified:   docs/opcodes.md modified:   rtl/instruction_decoder.sv

--- rtl/instruction_decoder.sv
+++ rtl/instruction_decoder.sv
@@ -33,7 +33,24 @@ module instruction_decoder
     decoder_B | decoder_I | decoder_J | decoder_R | decoder_R4 | decoder_S | decoder_U | decoder_X;
 
   always_comb begin  // decoder_B
-    decoder_B = '0;
+    decoder_B           = '0;
+    decoder_B.imm       = code_i[31] ? '1 : '0;
+    decoder_B.imm[12]   = code_i[31];
+    decoder_B.imm[11]   = code_i[7];
+    decoder_B.imm[10:5] = code_i[30:25];
+    decoder_B.imm[4:1]  = code_i[11:8];
+    decoder_B.imm[0]    = '0;
+    decoder_B.rs1       = code_i[19:15];
+    decoder_B.rs2       = code_i[24:20];
+    case (code_i[14:12])
+      3'd0: decoder_B.funct = BEQ;
+      3'd1: decoder_B.funct = BNE;
+      3'd4: decoder_B.funct = BLT;
+      3'd5: decoder_B.funct = BGE;
+      3'd6: decoder_B.funct = BLTU;
+      3'd7: decoder_B.funct = BGEU;
+      default decoder_B.funct = INVALID;
+    endcase
     if (decoder_B.funct == INVALID) decoder_B = '0;
   end
 
@@ -44,6 +61,14 @@ module instruction_decoder
 
   always_comb begin  // decoder_J
     decoder_J = '0;
+    decoder_J.imm = code_i[31] ? '1 : '0;
+    decoder_J.imm[20] = code_i[31];
+    decoder_J.imm[10:1] = code_i[30:21];
+    decoder_J.imm[11] = code_i[20];
+    decoder_J.imm[19:12] = code_i[19:12];
+    decoder_J.imm[0] = 0;
+    decoder_J.rd = code_i[11:7];
+    decoder_J.funct = JAL;
     if (decoder_J.funct == INVALID) decoder_J = '0;
   end
 
@@ -54,7 +79,7 @@ module instruction_decoder
 
   always_comb begin  // decoder_R4
     decoder_R4 = '0;
-    if (decoder_R4.funct == INVALID) ecoder_R4 = '0;
+    if (decoder_R4.funct == INVALID) decoder_R4 = '0;
   end
 
   always_comb begin  // decoder_S
@@ -68,7 +93,26 @@ module instruction_decoder
   end
 
   always_comb begin  // decoder_X
-    decoder_X = '0;
+    decoder_X      = '0;
+    decoder_X.csr  = code_i[31:20];
+    decoder_X.rd   = code_i[11:7];
+    decoder_X.rs1  = code_i[19:15];
+    decoder_X.uimm = code_i[19:15];
+    case (code_i)
+      1: decoder_X.funct = CSRRW;
+      2: decoder_X.funct = CSRRS;
+      3: decoder_X.funct = CSRRC;
+      5: decoder_X.funct = CSRRWI;
+      6: decoder_X.funct = CSRRSI;
+      7: decoder_X.funct = CSRRCI;
+      default: begin
+        case (code_i[31:7])
+          25'h0000000: decoder_X.funct = ECALL;
+          25'h0002000: decoder_X.funct = EBREAK;
+          default decoder_X.funct = INVALID;
+        endcase
+      end
+    endcase
     if (decoder_X.funct == INVALID) decoder_X = '0;
   end
 
